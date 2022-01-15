c19512e010e9eea502043a7d241e8bbe1cfbfacc
Alyssa Harris <amh302918@gmail.com>
2022-01-14 21:50:31 -0500

    added sessions

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
-responses = []
-
+RESPONSES_KEY = "responses"
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "aubbiedobbieboo"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
