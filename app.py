6e6e253d16edca30851fdc7f5623fe342366d67e
Alyssa Harris <amh302918@gmail.com>
2022-01-14 21:44:03 -0500

    Completed app

--- app.py
+++ app.py
@@ -1,10 +1,61 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey as survey
+
+responses = []
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "aubbiedobbieboo"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
 debug = DebugToolbarExtension(app)
 
 @app.route('/')
 def home_page():
-    return render_template("hello.html")
+    return render_template("start_page.html", survey=survey)
+
+@app.route("/start", methods=["POST"])
+def start_survey():
+    return redirect("/questions/0")
+
+@app.route("/answer", methods=["POST"])
+def handle_question():
+    """Save response and redirect to next question."""
+
+    choice = request.form['answer']
+    responses.append(choice)
+    if (len(responses) == len(survey.questions)):
+        # They've answered all the questions! Thank them.
+        return redirect("/complete")
+
+    else:
+        return redirect(f"/questions/{len(responses)}")
+
+
+@app.route("/questions/<int:qid>")
+def show_question(qid):
+    """Display current question."""
+
+    if (responses is None):
+        # trying to access question page too soon
+        return redirect("/")
+
+    if (len(responses) == len(survey.questions)):
+        # They've answered all the questions! Thank them.
+        return redirect("/complete")
+
+    if (len(responses) != qid):
+        # Trying to access questions out of order.
+        flash(f"Invalid question id: {qid}.")
+        return redirect(f"/questions/{len(responses)}")
+
+    question = survey.questions[qid]
+    return render_template(
+        "questions.html", question_num=qid, question=question)
+
+
+@app.route("/complete")
+def complete():
+    """Survey complete. Show completion page."""
+
+    return render_template("complete.html")
